05a1619072b297455805697b979fc8b9242aac36
Rajat <rajatraj2207@gmail.com>
2022-03-06 02:43:30 +0530

    Created first get route

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const mongoose=require('mongoose');
 
 const app=express();
 
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.set('view engine','ejs');
+
+app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
 
 const ArticleSchema= {
@@ -12,6 +20,17 @@ const ArticleSchema= {
 }
 
 const Article=mongoose.model("Article",ArticleSchema); 
+
+app.get('/articles',function(req,res){
+    Article.find({},function(err,results){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }else{
+            res.send(results);
+        }
+    }); 
+});
 
 app.listen(3000,function(){
     console.log("App running on port 3000");
